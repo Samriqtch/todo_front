b05d5b1780ea7d09209fee4a5deae11bfba7c6bc
Samson KLOUGAN <99410105+Samriqtch@users.noreply.github.com>
2023-12-05 19:04:44 +0000

    Mettre à jour un profil

--- lib/edit_user_page.dart
+++ lib/edit_user_page.dart
@@ -1,73 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:todo_front/user_service.dart';
 
+
 class EditUserPage extends StatefulWidget {
   final Map<String, dynamic> user;
 
   const EditUserPage({Key? key, required this.user}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _EditUserPageState createState() => _EditUserPageState();
 }
 
 class _EditUserPageState extends State<EditUserPage> {
   final _formKey = GlobalKey<FormState>();
-  late TextEditingController _nameController;
-  late TextEditingController _emailController;
+  late TextEditingController _firstNameController;
+  late TextEditingController _lastNameController;
+  late TextEditingController _ageController;
 
   @override
   void initState() {
     super.initState();
-    _nameController = TextEditingController(text: widget.user['name']);
-    _emailController = TextEditingController(text: widget.user['email']);
+    _firstNameController = TextEditingController(text: widget.user['firstName']);
+    _lastNameController = TextEditingController(text: widget.user['lastName']);
+    _ageController = TextEditingController(text: widget.user['age'].toString());
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Edit User'),
+        title: Text('Edit User'),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              controller: _nameController,
-              decoration: const InputDecoration(labelText: 'Name'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a name';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter an email';
-                }
-                return null;
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  UserService().updateUser(widget.user['id'], {
-                    'name': _nameController.text,
-                    'email': _emailController.text,
-                  });
-                  Navigator.pop(context);
-                }
-              },
-              child: const Text('Submit'),
-            ),
-          ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.person, size: 100),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      controller: _firstNameController,
+                      decoration: InputDecoration(
+                        labelText: 'First Name',
+                      ),
+                    ),
+                    TextFormField(
+                      controller: _lastNameController,
+                      decoration: InputDecoration(
+                        labelText: 'Last Name',
+                      ),
+                    ),
+                    TextFormField(
+                      controller: _ageController,
+                      decoration: InputDecoration(
+                        labelText: 'Age',
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          // Update the user data
+                          setState(() {
+                            widget.user['firstName'] = _firstNameController.text;
+                            widget.user['lastName'] = _lastNameController.text;
+                            widget.user['age'] = int.parse(_ageController.text);
+                          });
+
+                          // Pop the page to go back
+                          Navigator.of(context).pop();
+                        }
+                      },
+                      child: Text('Save'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
