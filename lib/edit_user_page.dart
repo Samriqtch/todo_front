ddfa14ea920c0084b48699c4374a4a7c1507c000
Samson KLOUGAN <99410105+Samriqtch@users.noreply.github.com>
2023-12-05 20:16:07 +0000

    Mise à jour des informations d'un utilisateur

--- lib/edit_user_page.dart
+++ lib/edit_user_page.dart
@@ -1,7 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:todo_front/user_service.dart';
-
-import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class EditUserPage extends StatefulWidget {
   final Map<String, dynamic> user;
@@ -29,7 +28,7 @@ class _EditUserPageState extends State<EditUserPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
+      appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () => Navigator.of(context).pop(),
@@ -48,21 +47,21 @@ class _EditUserPageState extends State<EditUserPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-                         Center(
-              child: Container(
-                width: 250,
-                height: 250,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  shape: BoxShape.rectangle,
-                  border: Border.all(
-                    color: Color.fromARGB(255, 32, 32, 32),
-                    width: 0.5,
+              Center(
+                child: Container(
+                  width: 250,
+                  height: 250,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    shape: BoxShape.rectangle,
+                    border: Border.all(
+                      color: Color.fromARGB(255, 32, 32, 32),
+                      width: 0.5,
+                    ),
                   ),
+                  child: Icon(Icons.person, size: 200),
                 ),
-                child: Icon(Icons.person, size: 200),
               ),
-            ),
               Form(
                 key: _formKey,
                 child: Column(
@@ -85,46 +84,70 @@ class _EditUserPageState extends State<EditUserPage> {
                         labelText: 'Age',
                       ),
                     ),
-ElevatedButton(
-  onPressed: () {
-    if (_formKey.currentState!.validate()) {
-      // Show a dialog to confirm the action
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Confirm'),
-            content: Text('Do you want to save the changes?'),
-            actions: <Widget>[
-              TextButton(
-                child: Text('Cancel'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-              TextButton(
-                child: Text('Save'),
-                onPressed: () {
-                  // Update the user data
-                  setState(() {
-                    widget.user['firstName'] = _firstNameController.text;
-                    widget.user['lastName'] = _lastNameController.text;
-                    widget.user['age'] = int.parse(_ageController.text);
-                  });
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          // Show a dialog to confirm the action
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('Confirm'),
+                                content: Text('Do you want to save the changes?'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: Text('Cancel'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                  TextButton(
+                                    child: Text('Save'),
+                                    onPressed: () async {
+                                      // Update the user data
+                                      setState(() {
+                                        widget.user['firstName'] = _firstNameController.text;
+                                        widget.user['lastName'] = _lastNameController.text;
+                                        widget.user['age'] = int.parse(_ageController.text);
+                                      });
 
-                  // Pop the dialog and the page to go back
-                  Navigator.of(context).pop();
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-  },
-  child: Text('Save'),
-),
+                                      // Send the updated data to the backend
+                                      final response = await http.put(
+                                        Uri.parse('http://192.168.1.72:3000/users/id}'),
+                                        headers: <String, String>{
+                                          'Content-Type': 'application/json; charset=UTF-8',
+                                        },
+                                        body: jsonEncode(<String, String>{
+                                          'firstName': _firstNameController.text,
+                                          'lastName': _lastNameController.text,
+                                          'age': _ageController.text,
+                                        }),
+                                      );
+
+                                      if (response.statusCode == 200) {
+                                        // If the server returns a OK response, then parse the JSON.
+                                        ScaffoldMessenger.of(context).showSnackBar(
+                                          SnackBar(content: Text('User updated successfully')),
+                                        );
+                                      } else {
+                                        // If the server did not return a OK response,
+                                        // then throw an exception.
+                                        throw Exception('Failed to update user');
+                                      }
+
+                                      // Pop the dialog and the page to go back
+                                      Navigator.of(context).pop();
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        }
+                      },
+                      child: Text('Save'),
+                    ),
                   ],
                 ),
               ),
@@ -135,4 +158,3 @@ ElevatedButton(
     );
   }
 }
-
